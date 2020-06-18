18f99e1e59c7ebeceaa5fa70e49155dcecf40a91
OLUWATIMILEHIN AKINLEYE <akinleyeoj@gmail.com>
2020-06-18 22:28:07 +0100

    install create-react-app and general setups

---
+++ src/data.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class data extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default data
