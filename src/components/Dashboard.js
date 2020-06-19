10a6819ebfdfbc2263f1a113962f2eab7182b667
OLUWATIMILEHIN AKINLEYE <akinleyeoj@gmail.com>
2020-06-19 15:55:55 +0100

    feat: update dashbord menu
    Add NavLink features to Home, newpoll, leaderboard and login page as per instructions

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom';
 
 export class Dashboard extends Component {
-    render() {
-        return (
-            <div>
-                Dashboard
-            </div>
-        )
-    }
+  render() {
+    return (
+      
+      <div className="ui fixed inverted menu">
+     <div className="ui container " >
+         
+      <NavLink to="/"
+            exact
+            className="header item"
+            activeClassName="active" > Home </NavLink>
+          
+          <NavLink to="/newpoll"
+            exact
+            className="item"
+            activeClassName="active" > New Poll </NavLink>
+         
+          <NavLink to="/leaderboard"
+            exact
+            className="item"
+            activeClassName="active" > Leader Board </NavLink>
+          
+          <NavLink to="/login"
+            exact
+            className="ui right floated item"
+            activeClassName="active" > Login </NavLink>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Dashboard
+export default Dashboard;
+
