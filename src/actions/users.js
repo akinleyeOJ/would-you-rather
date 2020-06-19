e3df5c0555aac60aa048763c807cd524201b299c
OLUWATIMILEHIN AKINLEYE <akinleyeoj@gmail.com>
2020-06-19 18:06:43 +0100

    chore: add updates to action and reducer functions

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,5 +1,15 @@
 export const GET_USERS = 'GET_USERS'
+export const USER_ADD_QUESTION = 'USER_ADD_QUESTION' 
+export const USER_ANSWER_QUESTION = 'USER_ANSWER_QUESTION'
 
 export const getUsers = (users) => {
   return { type: GET_USERS, users,};
+}
+
+export const userAddQuestion = (users) => {
+  return { type: USER_ADD_QUESTION, users,};
+}
+
+export const userAnswerQuestion = (users) => {
+  return { type: USER_ANSWER_QUESTION, users,};
 }
--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,10 +1,30 @@
-import { GET_USERS } from "../actions/users";
+import { GET_USERS, USER_ADD_QUESTION, USER_ANSWER_QUESTION } from "../actions/users";
+
 
 export default function users(state = {}, action) {
   switch (action.type) {
     case GET_USERS:
       return { ...state, ...action.users };
-
+      case USER_ADD_QUESTION:
+        const { id, author } = action;
+        return {
+          ...state, [author]: {...state[author],
+            questions: state[author].questions.concat(id)
+          }
+        };
+      
+      case USER_ANSWER_QUESTION:
+        const { authedUser, qid, answer } = action;
+        return {
+          ...state,
+          [authedUser]: {
+            ...state[authedUser],
+            answers: {
+              ...state[authedUser].answers,
+              [qid]: answer
+            }
+          }
+        };
       
     default:
       return state;
