456f4145ab2e09629c7af71900a2d839fdc26fd4
OLUWATIMILEHIN AKINLEYE <akinleyeoj@gmail.com>
2020-06-22 08:33:19 +0100

    chore: fix bugs, update newQuestions to redirect to Question page

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -6,84 +6,67 @@ import { handleAddQuestion } from  '../actions/questions'
 
 export class NewQuestion extends Component {	
 	state = {      
-    	optionOneText:'',
+  	optionOneText:'',
 		optionTwoText:'',
-		toHome: false
 	};
 
-	handleInputChange = (event, type) => {
-		const value = event.target.value;
 
-		this.setState((state) => {
-			return type === 'option1' ? {...state, optionOneText: value} : {...state, optionTwoText: value}
-		});
-	}
+  handleOnChange = (e, data) => {
+    this.setState({ [data.id]: data.value });
+  };
 
-	handleSubmit = (event) => {   
-    	event.preventDefault();
+  handleClick = async () => {
 
-    	const { dispatch } = this.props
+      const { authedUser: author, history, resetActiveIndexToZero } = this.props
     	const { optionOneText, optionTwoText} = this.state   
     
-    	dispatch(handleAddQuestion(
-      		optionOneText,
-      		optionTwoText
-    	))
-
-    	this.setState({
-        	optionOneText:'',
-			optionTwoText:'',
-			toHome: true
-      	})
-  	}
+      await this.props.handleAddQuestion({
+        optionOneText,
+        optionTwoText,
+        author
+      });
+      resetActiveIndexToZero();
+      history.push("/");
+    };
  
 	render() {
         const {authedUser, users} = this.props;
         const user = users[authedUser];
-		const { toHome } = this.state;
-
-		if (toHome) {
-			return <Redirect to='/dashboard' />
-		}
 
 		return (
 			<div>
-                <Card.Group centered>
+       <Card.Group centered>
             <Card style={{ width: "500px"}}>
                 <Card.Content>
                     <Image size="tiny" src={user.avatarURL}/>
                    
         <div >Hello {user.name},  Add New Poll Below</div>
-				<Form onSubmit={this.handleSubmit}>
+				<Form>
 					<div>Would you rather...</div>
 					<Input 
-						name="optionOneText"
+						id="optionOneText"
 						type="text"
 						placeholder="Please Enter Option 1"
 						value={this.state.optionOneText}
-						onChange={(event) => this.handleInputChange(event, 'option1')} />
+            onChange={this.handleOnChange} />
 					<div className="or">Or</div>
 					<Form>
-                        <Input 
-						name="optionTwoText"
+          <Input 
+						id="optionTwoText"
 						type="text"
 						placeholder="Please Enter Option 2"
 						value={this.state.optionTwoText}
-						onChange={(event) => this.handleInputChange(event, 'option2')} />
+						onChange={this.handleOnChange} />
                          </Form> 
                    <Message red style={{ color: "Red"},{floated: "centered"}}> Enter Both Options To Submit</Message>         
-                <Card.Content  extra>
-                    
-					<Button color="red" basic type="submit">Submit</Button>
-                   
-                   
-                     </Card.Content>
-                    
+        <Card.Content>       
+					<Button color="red" basic onClick={this.handleClick}>Submit</Button>
+        </Card.Content>          
 				</Form>
-                </Card.Content>
-                </Card>
-                </Card.Group>
-			</div> 
+      </Card.Content>
+     </Card>
+     </Card.Group>
+	</div> 
   		)
 	}
 }
@@ -93,4 +76,4 @@ const mapSatetToProps = state => {
     return {users: state.users, authedUser: state.authedUser};
 }
 
-export default connect(mapSatetToProps)(NewQuestion);
+export default connect(mapSatetToProps, {handleAddQuestion})(NewQuestion);
